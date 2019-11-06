7a0902fdbe7438c0a76faf330cd937ec9bcf16f3
Valentin <valentin.nerin@gmail.com>
2019-11-06 21:16:13 +0100

    Initial commit

---
+++ src/main/java/com/valentin/sarbacane/SarbacaneApplication.java
@@ -0,0 +1,13 @@
+package com.valentin.sarbacane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarbacaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarbacaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valentin/sarbacane/SarbacaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentin.sarbacane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarbacaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
